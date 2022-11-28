9bef4847276d4e3c2025a0a1cc39954a6c90371f
StefaniaSgreva <stefaniasgreva@gmail.com>
2022-11-28 15:45:26 +0100

    add Bootstrap table

--- index.php
+++ index.php
@@ -48,16 +48,61 @@ $hotels = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
     <title>PHP-Hotel</title>
 </head>
 
 <body>
-
-    <?php
+    <!-- <?php
     foreach ($hotels as $hotel) {
         echo '<div>' . $hotel['name'] . ' ' . $hotel['description'] . ' ' . $hotel['parking'] . ' ' . $hotel['vote'] . ' ' . $hotel['distance_to_center'] . '</div>';
     }
-    ?>
+    ?> -->
+    <div class="container">
+        <h1 class="text-center my-5">Hotels</h1>
+        <table class="table table-dark table-hover">
+            <thead>
+                <tr>
+                    <th scope="col">Name</th>
+                    <th scope="col">Description</th>
+                    <th scope="col">Parking</th>
+                    <th scope="col">Vote</th>
+                    <th scope="col">Distance to center</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php
+
+                foreach ($hotels as $hotel) { ?>
+                <tr>
+                    <td>
+                        <? echo $hotel['name'] ?>
+                    </td>
+                    <td>
+                        <? echo $hotel['description'] ?>
+                    </td>
+                    <td>
+                        <? echo $hotel['parking'] ?>
+                    </td>
+                    <td>
+                        <? echo $hotel['vote'] ?>
+                    </td>
+                    <td>
+                        <? echo $hotel['distance_to_center'] ?>
+                    </td>
+                </tr>
+                <?php }
+                ?>
+
+
+            </tbody>
+        </table>
+    </div>
+
+
 
 </body>
 
